37b801e14cbc7140642468f9a91991e9f5ebc8ef
HBhawe <harshalb97@outlook.com>
2025-06-25 13:53:43 +0200

    ternary operator rendering

--- 03-pizza-menu/pizza-menu/src/index.js
+++ 03-pizza-menu/pizza-menu/src/index.js
@@ -68,16 +68,19 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  //   const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObject={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later. :)</p>
       )}
     </main>
   );
@@ -105,11 +108,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
-          <p>We're open until {closeHour}. Come visit us or order online.</p>
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order online</button>
         </div>
+      ) : (
+        <p>
+          We're closed at the moment. Please visit us from {openHour} onwards.
+        </p>
       )}
     </footer>
   );
