1e67635f5175d355d954986e3e8b29adc9380444
HBhawe <harshalb97@outlook.com>
2025-07-03 11:56:02 +0200

    Children props for accordion

--- accordion/src/components/Accordion.js
+++ accordion/src/components/Accordion.js
@@ -2,10 +2,19 @@ import { AccordionItem } from "./AccordionItem";
 import React from "react";
 
 export function Accordion({ faqs }) {
+  const [curOpen, setCurOpen] = React.useState(null);
   return (
     <div className={"accordion"}>
       {faqs.map((faq, index) => (
-        <AccordionItem index={index} faq={faq} key={faq.title} />
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          index={index}
+          faq={faq}
+          key={faq.title}
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
     </div>
   );
--- accordion/src/components/AccordionItem.js
+++ accordion/src/components/AccordionItem.js
@@ -1,21 +1,20 @@
 import React from "react";
 
-export function AccordionItem({ index, faq }) {
-  const [open, setOpen] = React.useState(false);
-
+export function AccordionItem({ index, faq, curOpen, onOpen, children }) {
+  const isOpen = index === curOpen;
   function handleOpen() {
-    open ? setOpen(false) : setOpen(true);
+    onOpen(isOpen ? null : index);
   }
 
   return (
-    <div className={`${open ? "open" : ""}`}>
+    <div className={`${isOpen ? "open" : ""}`}>
       <ul className={`item`} onClick={() => handleOpen()}>
         <div className={"number"}>
           {index < 9 ? `0${index + 1}` : index + 1}
         </div>
         <div className={"title text"}>{faq.title}</div>
-        <div className={"icon"}>{open ? "-" : "+"}</div>
-        {open ? <div className="content-box">{faq.text}</div> : null}
+        <div className={"icon"}>{isOpen ? "-" : "+"}</div>
+        {isOpen ? <div className="content-box">{children}</div> : null}
       </ul>
     </div>
   );
