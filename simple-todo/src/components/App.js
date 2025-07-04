af6e5dab0ddd7ed4c28760059c32dd11884a2fbe
Harshal Bhawe <harshalb97@outlook.com>
2025-07-04 18:45:38 +0200

    Rewrite of app

--- simple-todo/src/components/App.js
+++ simple-todo/src/components/App.js
@@ -1,121 +1,65 @@
 import { useState } from "react";
-import { Button } from "./Button";
-import { FormAddToDoList } from "./FormAddToDoList";
-import { ToDoList } from "./ToDoList";
-
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
+import "../App.css";
 
 const initialToDos = [
-  {
-    name: "List 1",
-    list: [
-      { title: "todo1", isChecked: false },
-      { title: "todo2", isChecked: true },
-      { title: "todo3", isChecked: false },
-    ],
-    id: crypto.randomUUID(),
-  },
-  {
-    name: "List 2",
-    list: [
-      { title: "todo4", isChecked: false },
-      { title: "todo5", isChecked: true },
-      { title: "todo6", isChecked: true },
-    ],
-    id: crypto.randomUUID(),
-  },
+  { title: "todo1", isChecked: false, id: crypto.randomUUID() },
+  { title: "todo2", isChecked: true, id: crypto.randomUUID() },
+  { title: "todo3", isChecked: false, id: crypto.randomUUID() },
 ];
 
 function App() {
   // STATES
   const [todos, setTodos] = useState(initialToDos);
-  const [showAddTodo, setShowAddTodo] = useState(false);
-  const [selectedTodo, setSelectedTodo] = useState(null);
 
-  function handleShowAddToDoList() {
-    setShowAddTodo(!showAddTodo);
-  }
-
-  function handleAddToDoList(todo) {
-    setTodos((todos) => [...todos, todo]);
-    setShowAddTodo(false);
-  }
-
-  function handleSelection(todo) {
-    setSelectedTodo((cur) => (cur?.id === todo.id ? null : todo));
-    setShowAddTodo(false);
+  function handleAddToDo(newTodo) {
+    setTodos([...todos, newTodo]);
   }
 
   return (
-    <div className="app">
-      <div className="sidebar">
-        <ToDoList
-          todos={todos}
-          onSelection={handleSelection}
-          selectedTodo={selectedTodo}
-        />
-        {showAddTodo && <FormAddToDoList onAddToDoList={handleAddToDoList} />}
-        <Button onHandleClick={handleShowAddToDoList}>
-          {showAddTodo ? "Close" : "Add ToDoList"}
-        </Button>
+    <div className={"container"}>
+      <Header todos={todos}>ToDos</Header>
+      <div className="app">
+        <ToDoView todos={todos} />
       </div>
-
-      <div>{selectedTodo && <ToDoView selectedTodo={selectedTodo} />}</div>
     </div>
   );
 }
 
-function ToDoView({ selectedTodo }) {
-  return (
-    <div className={"tasks"}>
-      <ToDo selectedTodo={selectedTodo} />
-    </div>
+function Header({ todos, children }) {
+  const [todosRemaining, setTodosRemaining] = useState(
+    todos.filter((todo) => !todo.isChecked).length,
   );
+  // const todosRemaining = ;
+  console.log(todosRemaining);
+  return <h2>{children}</h2>;
 }
 
-function ToDo({ selectedTodo }) {
-  const [todoTasks, setTodoTasks] = useState(selectedTodo.list);
-
-  function handleToggleTodo(todo, title) {
-    console.log(todo, title);
-    // setTodoTasks((todo) =>
-    //   todo.map((task) =>
-    //     task.title === title ? (todo.isChecked = true) : todo.isChecked,
-    //   ),
-    // );
-  }
+function ToDoView({ todos }) {
   return (
-    <div className={"tasks"}>
-      {todoTasks.map((task) => (
-        <li key={task.title} className={"task-item"}>
-          <input
-            type="checkbox"
-            value={task.isChecked}
-            onChange={() => handleToggleTodo(task, task.title)}
-          ></input>
-          <p>{task.title}</p>
-        </li>
+    <ul>
+      {todos.map((item) => (
+        <ToDo todo={item} key={item.id} />
       ))}
-    </div>
+    </ul>
+  );
+}
+
+function ToDo({ todo }) {
+  const [checkedState, setCheckedState] = useState(todo.isChecked);
+
+  function handleToggle() {
+    setCheckedState(!checkedState);
+  }
+
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={checkedState}
+        onChange={() => handleToggle()}
+      ></input>
+      {todo.title}
+    </li>
   );
 }
 
--- simple-todo/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export function Button({ onHandleClick, children }) {
-  return (
-    <button className={"button"} onClick={onHandleClick}>
-      {children}
-    </button>
-  );
-}
