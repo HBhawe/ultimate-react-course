584f75bce1785cc3a63a5301a658628b05d74e3c
HBhawe <harshalb97@outlook.com>
2025-09-01 15:10:25 +0200

    Added react+vite project creation shell script

---
+++ create-react-vite-project.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'React+Vite project name:' projectName
+echo 'project name is :' $projectName
+
+# npm 7+, extra double-dash is needed:
+npm create vite@latest $projectName -- --template react
