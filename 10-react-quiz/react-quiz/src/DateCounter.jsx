48edf6f9e22fe196afee5d0e2aa7e122db488d0c
HBhawe <harshalb97@outlook.com>
2025-09-25 16:09:29 +0200

    Biome linting fixes

--- 10-react-quiz/react-quiz/src/DateCounter.jsx
+++ 10-react-quiz/react-quiz/src/DateCounter.jsx
@@ -1,69 +1,75 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
-	if (action.type === "inc") return state + 1;
-	if (action.type === "dec") return state - 1;
-	if (action.type === "setCount") return action.payload;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
-	// const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-	const [count, dispatch] = useReducer(reducer, 0);
+  const [count, dispatch] = useReducer(reducer, 0);
 
-	const [step, setStep] = useState(1);
+  const [step, setStep] = useState(1);
 
-	// This mutates the date object.
-	const date = new Date("june 21 2027");
-	date.setDate(date.getDate() + count);
+  // This mutates the date object.
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
 
-	const dec = function () {
-		dispatch({ type: "dec" });
-	};
+  const dec = () => {
+    dispatch({ type: "dec" });
+  };
 
-	const inc = function () {
-		dispatch({ type: "inc" });
-	};
+  const inc = () => {
+    dispatch({ type: "inc" });
+  };
 
-	const defineCount = function (e) {
-		// setCount(Number(e.target.value));
-		dispatch({ type: "setCount", payload: Number(e.target.value) });
-	};
+  const defineCount = (e) => {
+    // setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
+  };
 
-	const defineStep = function (e) {
-		setStep(Number(e.target.value));
-	};
+  const defineStep = (e) => {
+    setStep(Number(e.target.value));
+  };
 
-	const reset = function () {
-		// setCount(0);
-		setStep(1);
-	};
+  const reset = () => {
+    // setCount(0);
+    setStep(1);
+  };
 
-	return (
-		<div className="counter">
-			<div>
-				<input
-					type="range"
-					min="0"
-					max="10"
-					value={step}
-					onChange={defineStep}
-				/>
-				<span>{step}</span>
-			</div>
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
 
-			<div>
-				<button onClick={dec}>-</button>
-				<input value={count} onChange={defineCount} />
-				<button onClick={inc}>+</button>
-			</div>
+      <div>
+        <button onClick={dec} type="button">
+          -
+        </button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc} type="button">
+          +
+        </button>
+      </div>
 
-			<p>{date.toDateString()}</p>
+      <p>{date.toDateString()}</p>
 
-			<div>
-				<button onClick={reset}>Reset</button>
-			</div>
-		</div>
-	);
+      <div>
+        <button onClick={reset} type="reset">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
 export default DateCounter;
