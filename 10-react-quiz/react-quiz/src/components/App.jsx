f936decbe2535b81be26d2e78c0def0ba85f9e13
HBhawe <harshalb97@outlook.com>
2025-09-26 11:12:47 +0200

    Question styling, components, CSS

--- 10-react-quiz/react-quiz/src/components/App.jsx
+++ 10-react-quiz/react-quiz/src/components/App.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.jsx";
 import MainContent from "./MainContent.jsx";
-import { useEffect, useReducer } from "react";
+import Loader from "./Loader.jsx";
+import Error from "./Error.jsx";
+import StartScreen from "./StartScreen.jsx";
+import Question from "./Question.jsx";
+import NextButton from "./NextButton.jsx";
 
 const initialState = {
   questions: [],
 
   // loading/error/ready/active/finished
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -17,13 +25,37 @@ function reducer(state, action) {
     case "dataFailed":
       return { ...state, status: "error" };
 
+    case "start":
+      return { ...state, status: "active" };
+
+    case "newAnswer": {
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    }
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
     default:
       throw new Error(`Unknown action type ${action.type}`);
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  const numQuestions = questions.length;
 
   useEffect(() => {
     fetch("http://localhost:8000/questions", {})
@@ -36,8 +68,21 @@ function App() {
     <div className="app">
       <Header />
       <MainContent>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </MainContent>
     </div>
   );
---
+++ 10-react-quiz/react-quiz/src/components/NextButton.jsx
@@ -0,0 +1,19 @@
+function NextButton({ dispatch, answer }) {
+  if (answer === null)
+    return (
+      <button className="btn-disabled" disabled={true}>
+        Next
+      </button>
+    );
+
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "nextQuestion" })}
+    >
+      Next
+    </button>
+  );
+}
+
+export default NextButton;
