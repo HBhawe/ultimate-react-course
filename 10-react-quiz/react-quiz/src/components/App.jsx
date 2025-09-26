efdd312463094f4a2a36228ec00f4430d6bb0899
HBhawe <harshalb97@outlook.com>
2025-09-26 11:14:40 +0200

    Biome formatted

--- 10-react-quiz/react-quiz/src/components/App.jsx
+++ 10-react-quiz/react-quiz/src/components/App.jsx
@@ -8,84 +8,84 @@ import Question from "./Question.jsx";
 import NextButton from "./NextButton.jsx";
 
 const initialState = {
-  questions: [],
+	questions: [],
 
-  // loading/error/ready/active/finished
-  status: "loading",
-  index: 0,
-  answer: null,
-  points: 0,
+	// loading/error/ready/active/finished
+	status: "loading",
+	index: 0,
+	answer: null,
+	points: 0,
 };
 
 function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return { ...state, questions: action.payload, status: "ready" };
+	switch (action.type) {
+		case "dataReceived":
+			return { ...state, questions: action.payload, status: "ready" };
 
-    case "dataFailed":
-      return { ...state, status: "error" };
+		case "dataFailed":
+			return { ...state, status: "error" };
 
-    case "start":
-      return { ...state, status: "active" };
+		case "start":
+			return { ...state, status: "active" };
 
-    case "newAnswer": {
-      const question = state.questions.at(state.index);
+		case "newAnswer": {
+			const question = state.questions.at(state.index);
 
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-    }
+			return {
+				...state,
+				answer: action.payload,
+				points:
+					action.payload === question.correctOption
+						? state.points + question.points
+						: state.points,
+			};
+		}
 
-    case "nextQuestion":
-      return { ...state, index: state.index + 1, answer: null };
+		case "nextQuestion":
+			return { ...state, index: state.index + 1, answer: null };
 
-    default:
-      throw new Error(`Unknown action type ${action.type}`);
-  }
+		default:
+			throw new Error(`Unknown action type ${action.type}`);
+	}
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+	const [{ questions, status, index, answer }, dispatch] = useReducer(
+		reducer,
+		initialState,
+	);
 
-  const numQuestions = questions.length;
+	const numQuestions = questions.length;
 
-  useEffect(() => {
-    fetch("http://localhost:8000/questions", {})
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:8000/questions", {})
+			.then((res) => res.json())
+			.then((data) => dispatch({ type: "dataReceived", payload: data }))
+			.catch((err) => dispatch({ type: "dataFailed" }));
+	}, []);
 
-  return (
-    <div className="app">
-      <Header />
-      <MainContent>
-        {status === "loading" && <Loader />}
-        {status === "error" && <Error />}
-        {status === "ready" && (
-          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
-        )}
-        {status === "active" && (
-          <>
-            <Question
-              question={questions[index]}
-              dispatch={dispatch}
-              answer={answer}
-            />
-            <NextButton dispatch={dispatch} answer={answer} />
-          </>
-        )}
-      </MainContent>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Header />
+			<MainContent>
+				{status === "loading" && <Loader />}
+				{status === "error" && <Error />}
+				{status === "ready" && (
+					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+				)}
+				{status === "active" && (
+					<>
+						<Question
+							question={questions[index]}
+							dispatch={dispatch}
+							answer={answer}
+						/>
+						<NextButton dispatch={dispatch} answer={answer} />
+					</>
+				)}
+			</MainContent>
+		</div>
+	);
 }
 
 export default App;
