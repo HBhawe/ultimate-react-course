1550a79214009e027b3e1f8f976a2da0e2e1e2a1
HBhawe <harshalb97@outlook.com>
2025-09-29 16:56:14 +0200

    City component with effect

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./pages/AppLayout.jsx";
 import Homepage from "./pages/Homepage.jsx";
@@ -6,17 +6,42 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Product from "./pages/Product.jsx";
+import CityList from "./components/CityList.jsx";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+	const [cities, setCities] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+				setCities(data);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+
+		fetchCities();
+	}, [cities]);
+
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Homepage />} />
+				<Route index element={<Homepage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="/login" element={<Login />} />
 
-				<Route path="/app" element={<AppLayout />} />
+				<Route path="app" element={<AppLayout />}>
+					<Route index element={<CityList />} />
+					<Route path="cities" element={<CityList />} />
+					<Route path="countries" element={<p>List of countries</p>} />
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
