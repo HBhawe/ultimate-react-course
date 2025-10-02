5c8dc0de8c53db46812b3a36bb414f170587b8cf
HBhawe <harshalb97@outlook.com>
2025-10-02 17:10:26 +0200

    Programmatic navigation

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -8,57 +8,60 @@ import Pricing from "./pages/Pricing.jsx";
 import Product from "./pages/Product.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
 
 const BASE_URL = "http://localhost:8000";
 
 function App() {
-	const [cities, setCities] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-	useEffect(() => {
-		async function fetchCities() {
-			try {
-				setIsLoading(true);
-				const res = await fetch(`${BASE_URL}/cities`);
-				const data = await res.json();
-				setCities(data);
-			} catch (error) {
-				console.error(error);
-			} finally {
-				setIsLoading(false);
-			}
-		}
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
 
-		fetchCities();
-	}, []);
+    fetchCities();
+  }, []);
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Homepage />} />
-				<Route path="product" element={<Product />} />
-				<Route path="pricing" element={<Pricing />} />
-				<Route path="/login" element={<Login />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
 
-				<Route path="app" element={<AppLayout />}>
-					<Route
-						index
-						element={<CityList cities={cities} isLoading={isLoading} />}
-					/>
-					<Route
-						path="cities"
-						element={<CityList cities={cities} isLoading={isLoading} />}
-					/>
-					<Route
-						path="countries"
-						element={<CountryList cities={cities} isLoading={isLoading} />}
-					/>
-					<Route path="form" element={<p>Form</p>} />
-				</Route>
-				<Route path="*" element={<PageNotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
