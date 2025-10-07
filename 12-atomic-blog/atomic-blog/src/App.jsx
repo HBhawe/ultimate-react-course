74573b1845fb658a564703e290f71e27da94df56
HBhawe <harshalb97@outlook.com>
2025-10-07 13:38:50 +0200

    Performance testing with Profiler

--- 12-atomic-blog/atomic-blog/src/App.jsx
+++ 12-atomic-blog/atomic-blog/src/App.jsx
@@ -1,6 +1,7 @@
 import { faker } from "@faker-js/faker";
 import { createContext, useContext, useEffect, useState } from "react";
 import { PostProvider, usePosts } from "./PostContext.jsx";
+import Test from "./Test.jsx";
 
 function createRandomPost() {
 	return {
@@ -125,14 +126,16 @@ function List() {
 	const { posts } = usePosts();
 
 	return (
-		<ul>
-			{posts.map((post, i) => (
-				<li key={i}>
-					<h3>{post.title}</h3>
-					<p>{post.body}</p>
-				</li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{posts.map((post, i) => (
+					<li key={i}>
+						<h3>{post.title}</h3>
+						<p>{post.body}</p>
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
